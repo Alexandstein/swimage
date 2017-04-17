27a90b82705c028a8efd2dd2bab68ae86643f872
Alexandstein <alexand.stein@gmail.com>
2017-04-16 17:49:23 -0700

    Initial Commit

---
+++ SwImage/SwImage.h
@@ -0,0 +1,19 @@
+//
+//  SwImage.h
+//  SwImage
+//
+//  Created by Alex Stein on 4/16/17.
+//  Copyright © 2017 Alexander Stein. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwImage.
+FOUNDATION_EXPORT double SwImageVersionNumber;
+
+//! Project version string for SwImage.
+FOUNDATION_EXPORT const unsigned char SwImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwImage/PublicHeader.h>
+
+
